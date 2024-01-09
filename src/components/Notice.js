fa9bc979df0890d5781f8a73417b487fa2f4b885
ayjj2023 <ayjj2023@mymail.pomona.edu>
2024-01-08 22:21:01 -0800

    first version of 'notice' message, removed unnecessary files

---
+++ src/components/Notice.js
@@ -0,0 +1,13 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+function Notice(props) { 
+    return (
+        <Box sx = {{display: 'flex'}}>
+            <Typography fontWeight = "bold" variant = "h6">Notice:&nbsp;</Typography>
+            <Typography variant = "h6">{props.message}</Typography>
+        </Box>
+    )
+  }
+
+export default Notice;
---
+++ src/components/StatusBox.js
@@ -0,0 +1,7 @@
+function StatusBox() { 
+    return (
+        <div></div>
+    )
+  }
+
+export default StatusBox;

8c7f12a931e768df8ddd13f1c9340f3865ef8f63
ayjj2023 <ayjj2023@mymail.pomona.edu>
2024-05-06 00:50:15 -0700

    clearer layout using grid

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
 
 
 function LabeledBox({ label, height=90, children }) {
@@ -28,41 +30,94 @@ function LabeledBox({ label, height=90, children }) {
 }
 
 function App() {
-
   return (
     <div className="App">
-     <Stack direction = "column" spacing = {2} sx = {{paddingX: 2, paddingY: 2}}>
-      <LabeledBox label="Notice">
-        <Notice message = "hello this is a placeholder message"/>
-      </LabeledBox>
-      <Stack direction="row" spacing={2} sx = {{paddingX: 2, paddingY: 2}}>
-        <Stack direction="column" spacing={2} sx = {{width: 400, paddingX: 2, paddingY: 2}}>
+     <Stack direction = "column" spacing = {2} sx = {{paddingX: 2, paddingY: 5}}>
+      <Grid container spacing = {2}>
+        <Grid item xs = {12}>
+        <LabeledBox label="Notice">
+         <Notice message = "hello this is a placeholder message"/>
+        </LabeledBox>
+        </Grid>
+        <Grid item xs = {4}>
           <LabeledBox label = "Status" height = {100}>
-            <StatusBox status = "Observing" />
+             <StatusBox status = "Observing" />
           </LabeledBox>
+        </Grid>
+        <Grid item xs = {4}>
+          <LabeledBox label = "Sensor Status" height = {100}>
+             Nothing here yet.
+          </LabeledBox>
+        </Grid>
+        <Grid item xs = {4}>
+          <LabeledBox label = "Weather" height = {100}>
+             Nothing here yet.
+          </LabeledBox>
+        </Grid>
+        <Grid item xs = {4}>
           <LabeledBox label = "Schedule" height = {600}>
             <Schedule />
           </LabeledBox>
-          <LabeledBox label = "Clock">
-            <Clock />
-          </LabeledBox>
-        </Stack>
-        <Stack direction="column" spacing={2} sx = {{width: 400, paddingX: 2, paddingY: 2}}>
+        </Grid>
+        <Grid item xs = {8}>
           <LabeledBox label = "Targets" height = {400} width = {1000}> 
-            <Targets />
+             <Targets />
+           </LabeledBox>
+           <LabeledBox label = "Clock" height = {100}>
+             <Clock />
+           </LabeledBox>            
+        </Grid>
+        <Grid item xs = {4}>
+           <LabeledBox label = "Connections">
+             <Connection />
+           </LabeledBox>
+        </Grid>
+        <Grid item xs = {4}>
+          <LabeledBox label = "Log" height = {400}>
+            Nothing here yet.
           </LabeledBox>
-          <LabeledBox label = "Sederial Time">
-            <SiderealTime />
-          </LabeledBox>
-          <LabeledBox label = "Connections">
-            <Connection />
-          </LabeledBox>
-        </Stack>
-      </Stack>
+        </Grid>
+      </Grid>
      </Stack>
       
     </div>
   );
 }
+
+//   return (
+//     <div className="App">
+//      <Stack direction = "column" spacing = {2} sx = {{paddingX: 2, paddingY: 10}}>
+//       <LabeledBox label="Notice">
+//         <Notice message = "hello this is a placeholder message"/>
+//       </LabeledBox>
+//       <Stack direction="row" spacing={2} sx = {{paddingX: 2, paddingY: 2}}>
+//         <Stack direction="column" spacing={2} sx = {{width: 400, paddingX: 2, paddingY: 2}}>
+//           <LabeledBox label = "Status" height = {100}>
+//             <StatusBox status = "Observing" />
+//           </LabeledBox>
+//           <LabeledBox label = "Schedule" height = {600}>
+//             <Schedule />
+//           </LabeledBox>
+//           <LabeledBox label = "Clock">
+//             <Clock />
+//           </LabeledBox>
+//         </Stack>
+//         <Stack direction="column" spacing={2} sx = {{width: 400, paddingX: 2, paddingY: 2}}>
+//           <LabeledBox label = "Targets" height = {400} width = {1000}> 
+//             <Targets />
+//           </LabeledBox>
+//           <LabeledBox label = "Sederial Time">
+//             <SiderealTime />
+//           </LabeledBox>
+//           <LabeledBox label = "Connections">
+//             <Connection />
+//           </LabeledBox>
+//         </Stack>
+//       </Stack>
+//      </Stack>
+      
+//     </div>
+//   );
+// }
 
 export default App;
